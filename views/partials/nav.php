542d35f5b140a43bd15065375286fbf0d0937f4f
Andre Pinto <andrepinto.cg@gmail.com>
2024-05-21 11:32:25 -0300

    change nav icons

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -16,7 +16,7 @@
             </div>
             <div class="hidden md:block">
                 <div class="ml-4 flex items-center md:ml-6">
-                    <button type="button" class="relative h-8 w-8 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <button type="button" class="relative h-10 w-10 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <a target="_blank" href="https://github.com/andre-ap/crud-php"><img src="/views/img/code.png" alt="code"></a>
 
                     </button>
@@ -25,9 +25,12 @@
                     <div class="relative ml-3">
                         <div>
                             <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                <!-- <span class="absolute -inset-1.5"></span> -->
+                                <!-- <span class="sr-only">Open user menu</span> -->
+
+                                <a target="_blank" href="https://www.php.net/">
+                                    <img class="h-12 w-12 rounded-full" src="/views/img/php.png" alt="php">
+                                </a>
                             </button>
                         </div>
 
