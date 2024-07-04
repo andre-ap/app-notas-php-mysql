74ef8f89433a2c053771f76343304c6f6d6c4158
Andre Pinto <andrepinto.cg@gmail.com>
2024-07-04 14:44:58 -0300

    implement auth to notes

--- views/403.php
+++ views/403.php
@@ -1,4 +1,4 @@
-<?php $heading = '404'; ?> 
+<?php $heading = '403'; ?> 
 
 <?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
@@ -6,7 +6,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold">Desculpe, não foi possível encontrar a página procurada.</h1>
+        <h1 class="text-2xl font-bold">Você não está autorizado a acessar essa página.</h1>
         <p class= "mt-4">
             <a href="/" class="text-blue-700 underline">Voltar.</a>
         </p>
