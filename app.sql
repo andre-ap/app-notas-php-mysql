23d798a90b29694e13bc4e2396b38445a32cba96
Andre Pinto <andrepinto.cg@gmail.com>
2025-01-07 12:04:29 -0300

    add mysql commands file

---
+++ app.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE app;
+
+use app;
+
+-- Criação da tabela `user`
+CREATE TABLE user (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+) ENGINE = INNODB;
+
+-- Criação da tabela `notes`
+CREATE TABLE notes (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    body VARCHAR(255) NOT NULL,
+    user_id INT(11) NOT NULL,
+    CONSTRAINT fk_user__notes_id FOREIGN KEY (user_id)
+        REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE = INNODB;
+
+-- Inserir usuários na tabela `user`
+INSERT INTO user (name, email)
+VALUES 
+    ('Alice', 'alice@example.com'),
+    ('Bob', 'bob@example.com');
+
+-- Inserir notas para os usuários na tabela `notes`
+INSERT INTO notes (body, user_id)
+VALUES 
+    ('Lembrete: Reunião às 10h.', 1),  -- Nota para Alice
+    ('Comprar ingredientes para o jantar.', 1),  -- Outra nota para Alice
+    ('Finalizar relatório de vendas.', 2),  -- Nota para Bob
+    ('Marcar consulta médica.', 2);  -- Outra nota para Bob
--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return $config = [
     'database' => [
         'host' => 'localhost',
         'port' => '3306',
-        'dbname' => 'myapp',
+        'dbname' => 'app',
         'charset' => 'utf8'
     ]
 ];
