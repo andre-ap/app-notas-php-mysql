2e9889837d58a70f83d37f5a317419dec94845d2
Andre Pinto <andre.pinto.cefet@gmail.com>
2025-03-09 23:42:19 -0300

    create validations for new notes

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,10 +7,25 @@ $db = new Database($config['database']);
 $heading = 'Criar Nota';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+
+    $errros = [];
+
+    if (strlen($_POST['body']) === 0){
+        $errors['body'] = 'Uma mensagem de descrição é requerida.';
+    }
+
+    if (strlen($_POST['body']) > 1000){
+        $errors['body'] = 'A mensagem de descrição não pode ter mais de 1000 caracteres.';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+
+    
 }
 
 require 'views/note-create.view.php';
